2640f0ed65c38804be50763132643b177e7f6248
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-15 23:32:53 +0200

    add structure application

---
+++ src/test/java/com/codecool/crudinventorymanagementapp/CrudInventoryManagementAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.crudinventorymanagementapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudInventoryManagementAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
