1e64fdb3dcaaeb4f63bc4833bc737e83a619b748
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-19 11:12:13 +0200

    add method setTargetUrl()

--- src/main/java/com/codecool/crudinventorymanagementapp/security/SuccessHandler.java
+++ src/main/java/com/codecool/crudinventorymanagementapp/security/SuccessHandler.java
@@ -1,12 +1,14 @@
 package com.codecool.crudinventorymanagementapp.security;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 @Component
 public class SuccessHandler implements AuthenticationSuccessHandler {
@@ -15,5 +17,18 @@ public class SuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
             throws IOException {
+
+    }
+
+    private String setTargetUrl(Authentication authentication, HttpServletRequest request) {
+
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (roles.contains("ROLE_EMPLOYEE")) {
+            return request.getContextPath() + "/inventory";
+        } else if (roles.contains("ROLE_ADMIN")) {
+            return "/employee";
+        } else {
+            return "/login";
+        }
     }
 }
