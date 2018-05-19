3617478c837a30896319d62241953fbc9b710a14
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-19 11:19:44 +0200

    add method handle()

--- src/main/java/com/codecool/crudinventorymanagementapp/security/SuccessHandler.java
+++ src/main/java/com/codecool/crudinventorymanagementapp/security/SuccessHandler.java
@@ -1,7 +1,11 @@
 package com.codecool.crudinventorymanagementapp.security;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +17,31 @@ import java.util.Set;
 @Component
 public class SuccessHandler implements AuthenticationSuccessHandler {
 
+    private Log logger = LogFactory.getLog(this.getClass());
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
             throws IOException {
 
+        this.handle(request, response, authentication);
+    }
+
+    private void handle(HttpServletRequest request,
+                          HttpServletResponse response, Authentication authentication)
+            throws IOException {
+
+        String targetUrl = this.setTargetUrl(authentication, request);
+        if (response.isCommitted()) {
+            logger.debug(
+                    "Response already committed. Can't redirect to "
+                            + targetUrl);
+            return;
+        }
+
+        redirectStrategy.sendRedirect(request, response, targetUrl);
     }
 
     private String setTargetUrl(Authentication authentication, HttpServletRequest request) {
