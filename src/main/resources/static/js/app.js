c20216e6bf7131dff00109b42eadb97aae081354
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-20 16:21:03 +0200

    add method validateSurname()

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -16,3 +16,18 @@ function validateFirstName(ev) {
         statusName = "true";
     }
 }
+
+function validateSurname(ev) {
+    var surname = ev.target.value;
+    var errorBox = this.parentElement.nextElementSibling;
+
+    if(!validator.longerThan(3, surname)){
+        this.classList.add("error-border");
+        errorBox.textContent = "Surname should be longer than 3";
+        statusSurname = "false";
+    } else {
+        this.classList.remove("error-border");
+        errorBox.textContent = "";
+        statusSurname = "true";
+    }
+}
