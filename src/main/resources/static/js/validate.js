b2f3d2af8c4f823e1184d0a4c047d76d26a17053
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-20 16:15:44 +0200

    add method mailWithSpecialCharAt()

--- src/main/resources/static/js/validate.js
+++ src/main/resources/static/js/validate.js
@@ -2,4 +2,14 @@ validator = {
     longerThan: function (length, string) {
         return string.length > length;
     },
+
+    mailWithSpecialCharAt: function (email) {
+        var result = false;
+        for (var i = 0; i < email.length; i++) {
+            if (email.charAt(i) === '@') {
+                result = true;
+            }
+        }
+        return result;
+    },
 };
