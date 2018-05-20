7f50379ca820962ca1928d69841348d15cb2c2be
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-20 16:17:34 +0200

    add method mailWithSpecialCharDot()

--- src/main/resources/static/js/validate.js
+++ src/main/resources/static/js/validate.js
@@ -12,4 +12,24 @@ validator = {
         }
         return result;
     },
+
+    mailWithSpecialCharDot: function (email) {
+        var resultMail = false;
+        var indexApp = 0;
+        for (var i = 0; i < email.length; i++) {
+            if (email.charAt(i) === '@') {
+                resultMail = true;
+                indexApp = i;
+            }
+        }
+        if (resultMail === true) {
+            var resultDot = false;
+            for (var i = 0; i < email.length; i++) {
+                if (email.charAt(i) === '.' && i > indexApp + 1) {
+                    resultDot = true;
+                }
+            }
+        }
+        return resultDot;
+    },
 };
